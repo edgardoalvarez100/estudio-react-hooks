266cdb240abd757f8b8948927daf2e4b0789343d
Edgardo Alvarez <edgardo@livedesign.org>
2023-08-14 18:40:56 -0500

    “update”

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -2,12 +2,19 @@ import { useState } from "react"
 
 
 export const CounterApp = () => {
-    const [counter, setcounter] = useState(0)
+    const [counter, setCounter] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+    })
+    const { counter1, counter2, counter3 } = counter;
     return (
         <>
-            <h1>counter:</h1>
+            <h1>counter1: {counter1}</h1>
+            <h1>counter2: {counter2}</h1>
+            <h1>counter3: {counter3}</h1>
             <hr />
-            <button className="btn btn-primary" onClick={() => setcounter(counter + 1)}>+1</button>
+            <button className="btn btn-primary" onClick={() => setCounter({ ...counter, counter1: counter1 + 1 })}>+1</button>
         </>
     )
 }
