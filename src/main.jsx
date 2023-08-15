b3cc219cc4fca70d3de1ead94328785e63e936d9
Edgardo Alvarez <edgardo@livedesign.org>
2023-08-14 19:20:57 -0500

    “update”

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HookApp } from './HookApp'
+// import { HookApp } from './HookApp'
 
 import './index.css'
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HookApp />
+    <CounterWithCustomHook />
   </React.StrictMode>,
 )
