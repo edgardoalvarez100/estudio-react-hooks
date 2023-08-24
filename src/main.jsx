8d6b6e09764f5826ca0a1e198f76018a32710e8c
Edgardo Alvarez <edgardo@livedesign.org>
2023-08-24 16:56:36 -0500

    “update”

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,13 @@ import ReactDOM from 'react-dom/client'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
 // import { Memorize } from './06-memos/Memorize'
-import { CallbackHook } from './06-memos/CallbackHook'
+// import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <CallbackHook />
+  <Padre />
   // </React.StrictMode>,
 )
